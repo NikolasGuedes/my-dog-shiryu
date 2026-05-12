4b45968986b480f594bd3ad5a4618b7534eadf21
Nikolas Guedes da Silva <guedesnikolas@gmail.com>
2026-05-12 20:18:18 -0300

    feat: add Shiryu 3D model - first version

--- src/components/ThreeElement/ThreeElement.tsx
+++ src/components/ThreeElement/ThreeElement.tsx
@@ -1,30 +1,25 @@
-import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
+import { useGLTF } from "@react-three/drei";
 
 export default function ThreeElement() {
-  const boxRef = useRef<THREE.Mesh>(null);
+  const modelRef = useRef<THREE.Group>(null);
+  const { scene } = useGLTF("/3D/Shiryu.glb");
 
-  useFrame((_, delta) => {
-    if (boxRef.current) {
-      boxRef.current.rotation.y += delta;
-    }
-  });
 
   return (
     <>
+      <ambientLight intensity={0.8} />
       <directionalLight position={[5, 5, 5]} />
-      <mesh
-        ref={boxRef}
-        rotation={[
-          THREE.MathUtils.degToRad(45),
-          THREE.MathUtils.degToRad(90),
-          0,
-        ]}
+      <group
+        ref={modelRef}
+        position={[0, -1.35, 0]}
+        scale={1}
       >
-        <boxGeometry />
-        <meshStandardMaterial color="red" />
-      </mesh>
+        <primitive object={scene} />
+      </group>
     </>
   );
 }
+
+useGLTF.preload("/3D/Shiryu.glb");
